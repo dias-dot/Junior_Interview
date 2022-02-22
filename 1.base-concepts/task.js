4b76dd158132ff0890ae28a29c21a9be88515404
iamdiasmagzhan <iamdiasmagzhan@gmail.com>
2022-02-22 17:13:01 +0600

    baseconcepsts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,18 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr=[],disc,x1,x2,x;
+  disc = Math.pow(b,2)- 4 * a * c;
+  if(disc < 0){
+    arr.push("");
+  }
+  else if(disc == 0){
+    x= (-b)/(2*a);
+    arr.push(x);
+  }
+  else{
+    x1= (-b + Math.sqrt(disc))/(2*a);
+    x2= (-b - Math.sqrt(disc))/(2*a);
+    arr.push(x1,x2);
+  }
   return arr; // array
 }
 
