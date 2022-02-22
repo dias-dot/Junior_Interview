ec832f1b11ad4ac18b91aa0dc9cb72f5a4171f53
iamdiasmagzhan <iamdiasmagzhan@gmail.com>
2022-02-22 16:23:38 +0600

    массивы сравнение сохранить

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,10 +1,22 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+    let result;
+    if(arr1.length>arr2.length || arr1.length<arr2.length) {
+      result = "False";
+    } 
+    else {
+     for(let i = 0; i < arr1.length; i++){
+        if(arr1[i]==arr2[i]){
+            result = "True";
+        }
+        else {
+            result = "False";
+        }
+     }
+    }
+    return result; // boolean
+  
 }
+
 
 function advancedFilter(arr) {
   let resultArr;
